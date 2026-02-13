db9be8b02f7dfb6c9bb03ef3439367c687359dd9
David-Slezak <david.slezak.s@gyarab.cz>
2026-02-13 10:41:20 +0100

    A

--- prj/prj/settings.py
+++ prj/prj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$^1+&xd3+&sek-^tdh^6j7dt+5brgt-cid161&)9tzua*3m(gr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
