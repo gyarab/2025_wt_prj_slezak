f93e33e8ba7b7070c4adb0f1eaa133986f801caa
David-Slezak <david.slezak.s@gyarab.cz>
2026-03-13 09:50:46 +0100

    a

--- prj/app/views.py
+++ prj/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def render_home(request):
+    return render(request, "home.html")
+
+def render_about(request):
+    return render(request, "about.html")
--- prj/prj/urls.py
+++ prj/prj/urls.py
@@ -20,6 +20,6 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.render_homepage, name="homepage"),
+    path('', views.render_home, name="home"),
     path('about/', views.render_about, name="about"),
 ]
