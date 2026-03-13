d0b88d67181fd5e13c67800036f1f2e449da178d
David-Slezak <david.slezak.s@gyarab.cz>
2026-03-13 10:35:35 +0100

    a

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Person, Job, Workshop
 
-# Register your models here.
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Workshop)
+class WorkshopAdmin(admin.ModelAdmin):
+    pass
--- prj/app/models.py
+++ prj/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    #id
+    name = models.CharField(max_length=255)
+    birth_year = models.PositiveSmallIntegerField(blank=True,null=True)
+    death_year = models.PositiveSmallIntegerField(blank=True,null=True)
+    job = models.ForeignKey('Job', on_delete=models.SET_NULL, null=True)
+    teacher_workshop = models.ForeignKey('Workshop', on_delete=models.SET_NULL, null=True)
+
+class Job(models.Model):
+    #id
+    name = models.CharField(max_length=255)
+
+class Workshop(models.Model):
+    #id
+    name = models.CharField(max_length=255)
