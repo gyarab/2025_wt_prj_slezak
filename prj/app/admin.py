8271463a328c05ab21ffdbe3356eea70b57cc1e0
David-Slezak <david.slezak.s@gyarab.cz>
2026-05-29 08:47:34 +0200

    a

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,19 +1,27 @@
 from django.contrib import admin
-from .models import Person, Job, Workshop
+from .models import Person, Artwork, Gallery, Job, Workshop
 
-@admin.register(Movie)
-class MovieAdmin(admin.ModelAdmin):
-    list_display = ['title', 'year']
-    search_fields = ['title']
-
-
-@admin.register(Director)
-class DirectorAdmin(admin.ModelAdmin):
-    list_display = ['name', 'birth_year']
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ['name', 'birth_year', 'death_year', 'job']
     search_fields = ['name']
 
+@admin.register(Artwork)
+class ArtworkAdmin(admin.ModelAdmin):
+    list_display = ['name', 'material', 'creator']
+    search_fields = ['name']
 
-@admin.register(Genre)
-class GenreAdmin(admin.ModelAdmin):
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Workshop)
+class WorkshopAdmin(admin.ModelAdmin):
     list_display = ['name']
     search_fields = ['name']
